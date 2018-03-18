02a849803bda5dbcd3bf1df4649a38967f309e45
Saad Khattak <saadrustam@gmail.com>
2018-03-18 19:56:41 -0400

    Superbuild tl_common helper | TLOC_INCLUDE helper
    Superbuild now writes a tl_include_common.cmake file in the root
    directory to facilitate the inclusion of tl_base_ci's common header that
    has all the basis functionality.
    
    tl_common now has a TLOC_INCLUDE function that builds on include() by
    searching for a file in a list of directories and including the first
    one it finds.

--- include/tl_base_ci/tl_add_superbuild.cmake
+++ include/tl_base_ci/tl_add_superbuild.cmake
@@ -34,10 +34,14 @@ function(tl_add_superbuild)
   set(FILE_NAME "CMakeLists.txt")
   set(FILE_DEST "${PARSED_ARGS_DESTINATION}/${FILE_NAME}")
 
+  set(INCLUDE_FILE_NAME "tl_include_common.cmake")
+  set(INCLUDE_FILE_DEST "${PARSED_ARGS_DESTINATION}/${INCLUDE_FILE_NAME}")
+
   # -----------------------------------------------------------------------------
 
   TLOC_LOG_LINE  (STATUS)
   TLOC_LOG       (STATUS "Writing ${FILE_NAME} Superbuild to ${PARSED_ARGS_DESTINATION} ...")
+  TLOC_LOG       (STATUS "Writing ${INCLUDE_FILE_NAME} helper to ${PARSED_ARGS_DESTINATION} ...")
 
   # -----------------------------------------------------------------------------
 
@@ -126,6 +130,46 @@ add_dependencies(\${PROJ_NAME}_EXT tl_base_ci_EXT)
 if(EXISTS \"${PARSED_ARGS_DESTINATION}/CMakeLists_Post.cmake\")
   include(\"${PARSED_ARGS_DESTINATION}/CMakeLists_Post.cmake\")
 endif()
+
+# -----------------------------------------------------------------------------
+")
+
+  # -----------------------------------------------------------------------------
+
+  file(WRITE ${INCLUDE_FILE_DEST}
+"\
+# ----------------------------------
+# auto-generated file, do not modify
+# ----------------------------------
+
+# -----------------------------------------------------------------------------
+
+# This helper file allows you to include tl_common.cmake using the same function
+# found inside tl_common.cmake. Note that this file may be updated automatically
+# by tl_add_superbuild.cmake
+
+# -----------------------------------------------------------------------------
+
+find_package(tl_base_ci REQUIRED)
+get_target_property(tl_base_ci_INCLUDE_DIRECTORIES tl_base_ci
+  INTERFACE_INCLUDE_DIRECTORIES
+  )
+
+# -----------------------------------------------------------------------------
+# same function found in tl_common.cmake
+
+function(TLOC_INCLUDE_TEMP FILE PATHS)
+  foreach(PATH \${PATHS})
+    set(FULL_PATH \${PATH}\${FILE})
+    if (EXISTS \${FULL_PATH})
+      include(\${FULL_PATH})
+      return()
+    endif()
+  endforeach()
+  message(FATAL_ERROR \"Could not find file \${FILE} to include in paths \${PATHS}\")
+endfunction()
+
+TLOC_INCLUDE_TEMP(\"/tl_base_ci/tl_common.cmake\" \${tl_base_ci_INCLUDE_DIRECTORIES})
 
 # -----------------------------------------------------------------------------
 ")
--- include/tl_base_ci/tl_common.cmake
+++ include/tl_base_ci/tl_common.cmake
@@ -176,3 +176,18 @@ endfunction()
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
   TLOC_LOG(FATAL_ERROR "Please build in a directory that is not the source directory")
 endif()
+
+# -----------------------------------------------------------------------------
+# include helper
+
+function(TLOC_INCLUDE FILE PATHS)
+  foreach(PATH ${PATHS})
+    set(FULL_PATH ${PATH}${FILE})
+    if (EXISTS ${FULL_PATH})
+      TLOC_LOG_DETAIL(STATUS "Including file: ${FULL_PATH}")
+      include(${FULL_PATH})
+      return()
+    endif()
+  endforeach()
+  message(FATAL_ERROR "Could not find file ${FILE} to include in paths ${PATHS}")
+endfunction()
